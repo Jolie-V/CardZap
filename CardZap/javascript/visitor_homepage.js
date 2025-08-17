705094137c62a6cfeea0e42db19b73adf94d19ce
ProbablyCherry <157565366+ProbablyCherry@users.noreply.github.com>
2025-08-17 18:23:37 +0800

    Delete CardZap directory

--- CardZap/javascript/visitor_homepage.js
+++
@@ -1,9 +0,0 @@
-// Notification popup toggle
-const notificationBtn = document.querySelector('.notification-btn');
-const notificationPopup = document.querySelector('.notification-popup');
-
-if (notificationBtn && notificationPopup) {
-  notificationBtn.addEventListener('click', () => {
-    notificationPopup.classList.toggle('show');
-  });
-}
